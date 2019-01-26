c1fef02e116e90d04a9649490036e208d8d294b3
Victor Velev <velev.victor@yahoo.com>
2019-01-26 12:41:48 +0200

    [Fix] Too many requests for real-time data

--- webapp/userreporter/ml_models.py
+++ webapp/userreporter/ml_models.py
@@ -4,7 +4,7 @@ import pickle
 with open('../bin/fire_area.b', 'rb') as f:
     FIRE_AREA = pickle.load(f)
 
-with open('../bin/fire_clustering_200.b', 'rb') as f:
+with open('../bin/fire_clustering_150.b', 'rb') as f:
     FIRE_CLUSTERING = pickle.load(f)
 
 with open('../bin/fire_proba.b', 'rb') as f:
